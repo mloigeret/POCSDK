e0f4092daf13ba5d0ee3520e06231fff19e71f61
Manuel Loigeret <manuel@vc.telushealth.com>
2024-10-07 16:29:06 -0400

    change color

--- Sources/POCSDK/POCSDKViewController.swift
+++ Sources/POCSDK/POCSDKViewController.swift
@@ -25,7 +25,7 @@ class POCSDKViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .green
         setupUI()
     }
 

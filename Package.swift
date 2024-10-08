8ad7b64c5ec2016912ea17bf8f2e97d690e7465e
Manuel Loigeret <manuel@vc.telushealth.com>
2024-10-07 20:53:38 -0400

    remove development package

--- Package.swift
+++ Package.swift
@@ -14,14 +14,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/thii/SwiftHEXColors.git", from: "1.4.1"),
-        .package(path: "Development Packages/POCColor")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "POCSDK",
-            dependencies: ["SwiftHEXColors", "POCColor"]),
+            dependencies: ["SwiftHEXColors"]),
         .testTarget(
             name: "POCSDKTests",
             dependencies: ["POCSDK"]
